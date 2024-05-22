c51ba8508049a9e77d3100098e1e3e2541e02d54
Abhishek Kumar <Abhishek Kumar@ABHISHEK-KUMAR->
2024-05-22 11:10:41 +0530

    Add project files.

---
+++ RateOfChangeFunction/RateOfChange.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using ExcelDna.Integration;
+
+
+public class RateOfChangeFunction
+{
+    [ExcelFunction(Name = "func.RateOfChange", Description = "Rate of change")]
+    public static double RateOfChange(double startValue, double endValue)
+    {
+         
+        {
+            return (endValue - startValue) / startValue;
+        }
+       
+    }
+}
